0d12f1c2717ee9ed54a049e26205845c82760456
Дмитрий Павлов <dmitriyeternitypavlov@outlook.com>
2021-06-03 23:52:04 +0700

    Создание модели приложения, добавление классов модели, создание базы данных приложения и миграций.

---
+++ SimpleTrader.Domain/Models/Asset.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.Domain.Models
+{
+    public class Asset
+    {
+        public int Id { get; set; }
+        public Account Account { get; set; }
+        public Stock Stock { get; set; }
+        public int Shares { get; set; }
+    }
+}
---
+++ SimpleTrader.EntityFrameworkCore/SimpleTraderDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using SimpleTrader.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.EntityFrameworkCore
+{
+    public class SimpleTraderDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AssetTransaction> AssetTransactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AssetTransaction>().OwnsOne(a => a.Stock);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=SimpleTraderDB;Trusted_Connection=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
