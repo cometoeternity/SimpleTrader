f888e6fd6eac32be69f223b149b1e045ac29b95f
Дмитрий Павлов <dmitriyeternitypavlov@outlook.com>
2021-06-07 16:22:48 +0700

    Добавление слоя пользовательского интерфейса, библиотек FontAwesome5 и Material Design In XAML.

---
+++ SimpleTrader.WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using SimpleTrader.WPF.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.WPF.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+    }
+}
