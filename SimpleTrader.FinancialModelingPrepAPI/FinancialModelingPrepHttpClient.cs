444cfc4ae1cd4e8fbf367dc98a4ae1fb5d39d49b
Дмитрий Павлов <dmitriyeternitypavlov@outlook.com>
2021-06-22 16:23:28 +0700

    Настройка блока Навигации в главном приложении.

---
+++ SimpleTrader.FinancialModelingPrepAPI/FinancialModelingPrepHttpClient.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.FinancialModelingPrepAPI
+{
+    public class FinancialModelingPrepHttpClient : HttpClient
+    {
+        public FinancialModelingPrepHttpClient()
+        {
+            this.BaseAddress = new Uri("https://financialmodelingprep.com/api/v3");
+        }
+        public async Task<T> GetAsync<T>(string uri)
+        {
+            HttpResponseMessage responce = await GetAsync($"{uri}?=apikey=ded7fbeb58821884e6ee9eafd066eaaf");
+            string jsonResonse = await responce.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(jsonResonse);
+        }
+    }
+}
--- SimpleTrader.WPF/ViewModels/MajorIndexViewModel.cs
+++ SimpleTrader.WPF/ViewModels/MajorIndexViewModel.cs
@@ -1,10 +1,5 @@
 ﻿using SimpleTrader.Domain.Models;
 using SimpleTrader.Domain.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SimpleTrader.WPF.ViewModels
 {
